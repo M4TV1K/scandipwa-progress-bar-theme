d3187b9b7222da4c456602cc5d5cd12c36dfad06
m4tv1k <matvikfake@gmail.com>
2021-03-22 17:57:05 +0100

    Committing progress line animation

--- src/component/ProgressLine/ProgressLine.js
+++ src/component/ProgressLine/ProgressLine.js
@@ -1,15 +1,36 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef } from 'react';
 
 /** @namespace ProgressBarTheme/Component/ProgressLine/ProgressLine */
 // eslint-disable-next-line
 export const ProgressLine = ({ active, last = false }) => {
+    // eslint-disable-next-line
+    const filledBar = 100;
+    const animationSpeed = 1;
+    const lineRef = useRef();
+    const animateFilling = () => {
+        // eslint-disable-next-line
+        let fill = 0;
+        const animation = () => {
+            requestAnimationFrame(() => {
+                // eslint-disable-next-line
+                lineRef.current.style.backgroundImage = `linear-gradient(to right, darkred ${fill}%, lightgrey ${filledBar - fill}%)`;
+                fill += animationSpeed;
+                if (fill <= filledBar) {
+                    animation();
+                }
+            });
+        };
+
+        animation();
+    };
+
     useEffect(() => {
+        if (active) {
+            animateFilling();
+        }
     }, [active]);
     // eslint-disable-next-line
     let className = 'progress-line';
-    if (active) {
-        className += ' activated-line';
-    }
     if (last) {
         className += ' last-line';
     } else {
@@ -17,7 +38,7 @@ export const ProgressLine = ({ active, last = false }) => {
     }
 
     return (
-        <div className={ className } />
+        <div ref={ lineRef } className={ className } />
     );
 };
 
